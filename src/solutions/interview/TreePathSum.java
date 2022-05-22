1d5a0bcd6a2fc230e8dff9f3d939c159712d38fa
Lenard Bartha <lenb@jbdev.com>
2022-05-22 16:32:32 -0700

    Update TreePathSum.java

--- src/solutions/interview/TreePathSum.java
+++ src/solutions/interview/TreePathSum.java
@@ -4,8 +4,16 @@ import solutions.interview.Models.TreeNode;
 
 public class TreePathSum {
     public static boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) {
+        return getSum(root, targetSum, 0);
+    }
 
+    public static boolean getSum(TreeNode root, int targetSum, int sum) {
+        if (root.left == null && root.right == null && targetSum == sum) {
+            return true;
         }
+
+        sum += root.data;
+
+        return getSum(root.left, targetSum, sum) || getSum(root.right, targetSum, sum);
     }
 }
